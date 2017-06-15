b1882a081fa074f467d1685594fa4b9a8f14cdde
narendradivi <narendraavula2@gmail.com>
2017-06-15 11:42:02 +0530

    custom django model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    description = models.CharField(max_length=100, default='')
+    city = models.CharField(max_length=100,default='')
+    website = models.URLField(default='')
+    phone = models.IntegerField(default=0)
